ce7c398c6e05220aa9ef2f7f97c2ea1b47659613
Rafi372932883 <rxrafi669@gmail.com>
2024-12-10 08:59:32 +0600

    Update leave.js

--- scripts/events/leave.js
+++ scripts/events/leave.js
@@ -13,14 +13,14 @@ module.exports.run = async function({ api, event, Users, Threads }) {
 	const { threadID } = event;
 	const data = global.data.threadData.get(parseInt(threadID)) || (await Threads.getData(threadID)).data;
 	const name = global.data.userName.get(event.logMessageData.leftParticipantFbId) || await Users.getNameUser(event.logMessageData.leftParticipantFbId);
-	const type = (event.author == event.logMessageData.leftParticipantFbId) ? "লিভ নেউয়ার জন্য ধন্যবাদ 🤢" : "Kicked by Administrator";
+	const type = (event.author == event.logMessageData.leftParticipantFbId) ? "ᴡʜʏ ᴅɪᴅ ᴋʜᴀɴᴋɪʀ ᴘᴏʟᴀ ʟᴇᴀᴠᴇ ᴛʜᴇ ɢʀᴏᴜᴘ 🙂❓ " : "Kicked by Administrator";
 	const path = join(__dirname, "nayan", "leaveGif");
 	const gifPath = join(path, `l.gif`);
 	var msg, formPush
 
 	if (existsSync(path)) mkdirSync(path, { recursive: true });
 
-	(typeof data.customLeave == "undefined") ? msg = "তুই {name} গ্রুপে থাকার যোগ্য না আবাল .\n\n{type} " : msg = data.customLeave;
+	(typeof data.customLeave == "undefined") ? msg = " {name} ᴡʜʏ ᴄᴏᴍᴇ ɪꜰ ʏᴏᴜ ᴅᴏɴ'ᴛ ᴅᴇꜱᴇʀᴠᴇ ᴛᴏ ʙᴇ ɪɴ ᴛʜᴇ ɢʀᴏᴜᴘ🙂🖕 .\n\n{type} " : msg = data.customLeave;
 	msg = msg.replace(/\{name}/g, name).replace(/\{type}/g, type);
 
 	if (existsSync(gifPath)) formPush = { body: msg, attachment: createReadStream(gifPath) }
